cb04f1bd200f9a71dc2dce41ce8d05add46cc650
Abundance <sayantan.tapadar9@gmail.com>
2023-10-25 00:35:10 +0530

    added movePiece funtion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,13 @@ fun main() {
     val chess = ChessEngine()
 
     chess.setup()
-    chess.printBoard()
 
-    var list = chess.availableMoves("e7")
-    println(list)
+    chess.movePiece("e2", "e4")
+    chess.movePiece("e7", "e5")
+    chess.movePiece("e5", "e4")
+
+
+//    val list = chess.availableMoves("e5")
+//    println(list)
 
 }
